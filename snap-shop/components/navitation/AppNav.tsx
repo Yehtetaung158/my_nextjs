1657058d8004204ca583920af3fcb63eee552b37
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-03-16 00:40:22 +0630

    dropDownMenu_done

--- snap-shop/components/navitation/AppNav.tsx
+++ snap-shop/components/navitation/AppNav.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import NavLogo from "./NavLogo";
 import UserButton from "./UserButton";
+import useSessionData from "./useSessionData";
 
 const AppNav = () => {
+  const  session  = useSessionData();
+  console.log("I am session data in app nav ---------->", session);
+  useEffect(() => {
+    console.log("I am session data in app nav useEffect ---------->", session)
+  }, [session]);  
   return (
-    <div className="flex items-center justify-between w-full bg-red-300">
+    <div className="flex items-center justify-between w-full ">
       <NavLogo />
       <UserButton />
     </div>
--- snap-shop/components/navitation/UserButton.tsx
+++ snap-shop/components/navitation/UserButton.tsx
@@ -4,21 +4,73 @@ import { Button } from "../ui/button";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import useSessionData from "./useSessionData";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
+import { LogOut, Settings, Truck } from "lucide-react";
+import { use, useEffect } from "react";
 
 const UserButton = () => {
-  const { session } = useSessionData();
+  const { session, status } = useSessionData();
+  console.log("I am session data in UserButton ---------->", session?.user?.email);
+  useEffect(() => {
+    console.log("I am session data in UserButton useEffect ---------->", session?.user?.email)
+  }
+  , [session?.user?.email]);
 
-  console.log("I am user", session);
   return (
-    <div className=" flex gap-2 items-center">
-      {session?.user?.email && <p>{session?.user?.email}</p>}
+    <div className=" flex justify-center items-center">
       {session?.user?.email ? (
-        <Button variant="destructive" onClick={() => signOut()}>
-          Logout
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src={session?.user?.image!} />
+              <AvatarFallback className=" bg-primary text-white w-full h-full flex items-center justify-center">
+                {session?.user?.name?.slice(0, 2).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="p-2">
+            <div className=" flex gap-2 bg-primary p-2 rounded-lg items-center justify-center">
+              <Avatar>
+                <AvatarImage src={session?.user?.image!} />
+                <AvatarFallback className=" bg-white text-primary font-bold w-full h-full flex items-center justify-center">
+                  {session?.user?.name?.slice(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col gap-2 text-white">
+                <h3>{session?.user?.name}</h3>
+                <h3>{session?.user?.email}</h3>
+              </div>
+            </div>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className=" cursor-pointer">
+              <Truck className="w-5 h-5 mr-3" />
+              <span className=" text-sm font-medium">My Orders</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem className=" cursor-pointer">
+              <Settings className="w-5 h-5 mr-3" />
+              <span className=" text-sm font-medium">Setting</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className=" cursor-pointer text-red-600"
+              onClick={() => signOut()}
+            >
+              <LogOut className="w-5 h-5 mr-3" />
+              <span className=" text-sm font-medium">Log out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
+
         <Button asChild>
-          <Link href="/auth/login">Login</Link>
+          <Link href="/auth/login">{status === "loading" ? "Loading..." : "Login"}</Link>
         </Button>
       )}
     </div>
