ccf6aa398b5e2de520eb4898077ac56bfb5965e5
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-04-23 20:11:00 +0630

    nav-user-btn-fixed

--- snap-shop/components/navitation/AppNav.tsx
+++ snap-shop/components/navitation/AppNav.tsx
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import NavLogo from "./NavLogo";
 import UserButton from "./UserButton";
 import CartBtn from "../cart/cart-btn";
+import { auth } from "@/server/auth";
 
 const AppNav = async () => {
+  const session = await auth();
   return (
     <div className="flex items-center justify-between w-full ">
       <NavLogo />
       <div className="flex items-center gap-4 cursor-pointer">
         <CartBtn />
-        <UserButton />
+        <UserButton user={session?.user} expires={session?.expires!} />
       </div>
     </div>
   );
--- snap-shop/components/navitation/UserButton.tsx
+++ snap-shop/components/navitation/UserButton.tsx
@@ -1,85 +1,84 @@
 "use client";
-// import React from 'react'
+
+import { Session } from "next-auth";
 import { Button } from "../ui/button";
-import { SessionProvider, signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { LogIn, LogOut, Settings, Truck } from "lucide-react";
 import {
+  DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import { Avatar, AvatarImage } from "../ui/avatar";
-import { AvatarFallback } from "@radix-ui/react-avatar";
-import { LogOut, Settings, Truck } from "lucide-react";
-import { redirect } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useRouter } from "next/navigation";
 
-const UserButton = () => {
-  const { data: session, status } = useSession();
-  const user = session?.user;
+const UserButton = ({ user }: Session) => {
+  const router = useRouter();
+
   return (
-    <SessionProvider session={session} refetchInterval={5}>
-      <div className=" flex justify-center items-center">
-        {user ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger>
+    <div>
+      {user?.email ? (
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger className="ring-0">
+            <Avatar>
+              <AvatarImage src={user.image!} />
+              <AvatarFallback className="bg-primary text-white font-bold">
+                {user.name?.slice(0, 2).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="p-4">
+            <div className="flex gap-2 p-4 border-2 border-black/10 rounded-lg items-center mb-4 cursor-pointer hover:scale-95 transition-all duration-300 ease-in-out">
               <Avatar>
                 <AvatarImage src={user.image!} />
-                <AvatarFallback className="bg-primary text-white w-full h-full flex items-center justify-center">
+                <AvatarFallback className="bg-white text-primary font-bold">
                   {user.name?.slice(0, 2).toUpperCase()}
                 </AvatarFallback>
               </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="p-2">
-              <div className=" flex gap-2 bg-primary p-2 rounded-lg items-center justify-center">
-                <Avatar>
-                  <AvatarImage src={user.image!} />
-                  <AvatarFallback className=" bg-white text-primary font-bold w-full h-full flex items-center justify-center">
-                    {user?.name?.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col gap-2 text-white">
-                  <h3>{user?.name}</h3>
-                  <h3>{user?.email}</h3>
-                </div>
+              <div>
+                <h3 className="font-bold text-sm">{user.name}</h3>
+                <p className="text-sm font-medium">{user.email}</p>
               </div>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem className=" cursor-pointer">
-                <Truck className="w-5 h-5 mr-3" />
-                <span className=" text-sm font-medium">My Orders</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className=" cursor-pointer"
-                onClick={() => redirect("/dashboard/settings")}
-              >
-                <Settings className="w-5 h-5 mr-3" />
-                <span className=" text-sm font-medium">Setting</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className=" cursor-pointer text-red-600"
-                onClick={() => signOut()}
-              >
-                <LogOut className="w-5 h-5 mr-3" />
-                <span className=" text-sm font-medium">Log out</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        ) : (
-          <>
-            {status === "unauthenticated" ? (
-              <Button asChild>
-                <Link href="/auth/login">
-                  <span className="text-sm font-medium">Login</span>
-                </Link>
-              </Button>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </>
-        )}
-      </div>
-    </SessionProvider>
+            </div>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer group hover:bg-primary/10"
+              onClick={() => router.push("/dashboard/orders")}
+            >
+              <Truck className="w-5 h-5 mr-3 group-hover:translate-x-1 group-hover:text-primary transition-all duration-300 ease-in-out" />
+              <span className="text-sm font-medium">My Orders</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer group hover:bg-primary/10"
+              onClick={() => router.push("/dashboard/settings")}
+            >
+              <Settings className="w-5 h-5 mr-3 group-hover:rotate-180 group-hover:text-primary transition-all duration-300 ease-in-out" />
+              <span className="text-sm font-medium">Setting</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer  group hover:bg-red-200"
+              onClick={() => signOut()}
+            >
+              <LogOut className="w-5 h-5 mr-3 group-hover:translate-x-1 group-hover:scale-90  transition-all duration-300 ease-in-out group-hover:text-red-600 " />
+              <span className="text-sm font-medium group-hover:text-red-600 transition-all duration-300">
+                Logout
+              </span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <>
+          <Button asChild>
+            <Link href={"/auth/login"} className="space-x-4">
+              <LogIn size={16} /> <span>Login</span>
+            </Link>
+          </Button>
+        </>
+      )}
+    </div>
   );
 };
 
