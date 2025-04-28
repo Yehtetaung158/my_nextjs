b9f3a6605a3624bc317f0863c0a6b81b267c4cc2
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-04-29 01:25:33 +0630

    optimize

--- snap-shop/server/index.ts
+++ snap-shop/server/index.ts
@@ -4,8 +4,5 @@ import * as schema from "@/server/schema";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-// const sql = neon("postgresql://snap-shop_owner:npg_ljZTM6hYRe7a@ep-sweet-flower-a8pmb61j-pooler.eastus2.azure.neon.tech/snap-shop?sslmode=require");
-const sql = neon(
-  "postgresql://neondb_owner:npg_g6LBim2ATPvE@ep-steep-silence-a11xvhqr-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-);
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
